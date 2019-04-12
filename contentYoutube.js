d32909af84b141a4064e018ee6e8c723652ae4cf
Sabri Nu <sabrinuzi@gmail.com>
2019-04-12 18:06:34 +0200

    Upload files

---
+++ contentYoutube.js
@@ -0,0 +1,19 @@
+	var storage = chrome.storage.sync;
+	var storedSitesArray;
+	var siteStorageKey = "sites";
+
+	chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
+		
+		if (request.command == "getNext"){
+			var url = '';
+			var nextlink= $("#thumbnail").attr('href');
+
+			if( typeof nextlink !== 'undefined' && nextlink !=  'undefined' ){
+				url = "https://www.youtube.com" + nextlink;
+			}
+
+			sendResponse({nextUrl: url, refreshTab: request.tabId});
+		}
+		
+  });
+ 
